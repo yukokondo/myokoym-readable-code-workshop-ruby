fe0552e4fa7ad12acc93037b2b01625374a3227e
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-05 14:35:50 +0900

    spec6: extract Recipe class

--- recipe.rb
+++ recipe.rb
@@ -1,10 +1,18 @@
 #!/usr/bin/env ruby
 
+class Recipe
+  attr_reader :id, :name
+  def initialize(id, name)
+    @id = id
+    @name = name
+  end
+end
+
 def read_lines(data_path)
   recipes = []
   File.open(data_path, "r") do |file|
-    file.each_line do |line|
-      recipes << line
+    file.each_line.with_index do |line, i|
+      recipes << Recipe.new(i + 1, line)
     end
   end
   recipes
@@ -14,5 +22,5 @@ data_path = ARGV[0]
 recipes = read_lines(data_path)
 
 recipes.each_with_index do |recipe, i|
-  puts "#{i + 1}: #{recipe}"
+  puts "#{recipe.id}: #{recipe.name}"
 end
